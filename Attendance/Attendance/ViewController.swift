7920b2df3073685011f80a94e3a3778e20fe805e
gyeom0919 <leegyeom09@gmail.com>
2022-11-03 12:08:11 +0900

    뷰컨 수정

--- Attendance/Attendance/ViewController.swift
+++ Attendance/Attendance/ViewController.swift
@@ -1,45 +1,14 @@
 import UIKit
 import SnapKit
 import Then
-import Moya
 
 class ViewController: UIViewController {
-    @IBOutlet weak var textButton: UIButton!
-    var userService = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func ButtonTap() {
-//        userService.request(.signUp(name: "testUser", password: "123456")) { res in
-//            switch res {
-//            case .success(let result):
-//                switch result.statusCode {
-//                case 200:
-//                    print("succes")
-//                default:
-//                    print(result.statusCode)
-//                }
-//            case .failure(_):
-//                print("🆘Net error")
-//            }
-//        }
-        
-        userService.request(.logIn(name: "testUser", password: "123456")) { res in
-            switch res {
-            case .success(let result):
-                switch result.statusCode {
-                case 200:
-                    print("login succes")
-                default:
-                    print(result.statusCode)
-                }
-            case .failure(_):
-                print("🆘Net error")
-            }
-        }
-    }
+
 }
 
